c1f3ef3e6ae2eb4e13e23771a290bc8093b966b6
cwaldron <chris@chrissolutions.com>
2017-09-10 18:57:59 +1200

    Cleaned up logic in splash screen that starts the main activity.

--- DroneLander/DroneLander.Android/SplashActivity.cs
+++ DroneLander/DroneLander.Android/SplashActivity.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using System.Threading.Tasks;
 
 namespace DroneLander.Droid
@@ -16,24 +7,15 @@ namespace DroneLander.Droid
     [Activity(Label = "Drone Lander", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
-        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
-        {
-            base.OnCreate(savedInstanceState, persistentState);
-        }
-
         protected override void OnResume()
         {
             base.OnResume();
 
-            Task startupWork = new Task(async () =>
+            var startupWork = new Task(async () =>
             {
                 await Task.Delay(300);
+                RunOnUiThread(() => StartActivity(new Intent(Application.Context, typeof(MainActivity))));
             });
-
-            startupWork.ContinueWith(t =>
-            {
-                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
-            }, TaskScheduler.FromCurrentSynchronizationContext());
 
             startupWork.Start();
         }
